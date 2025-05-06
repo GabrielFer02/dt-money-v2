18fee961d762a5d62702f68e41262a17b5b46c0d
Gabriel Fernando <gf313074@gmail.com>
2025-05-05 21:48:04 -0300

    create income button and outcome button

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import style from "./NewTransactionModal.module.css";
-import { X } from "phosphor-react";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -11,13 +11,24 @@ export function NewTransactionModal() {
         <Dialog.Title>Nova Transação</Dialog.Title>
 
         <Dialog.Close className={style.closeButton}>
-          <X />
+          <X size={24} />
         </Dialog.Close>
 
         <form action="">
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <div className={style.transactionType}>
+            <button className={style.transactionTypeButton}>
+              <ArrowCircleUp size={24} className={style.income} />
+              Entrada
+            </button>
+            <button className={style.transactionTypeButton}>
+              <ArrowCircleDown size={24} className={style.outcome} />
+              Saída
+            </button>
+          </div>
 
           <button type="submit">Cadastrar</button>
         </form>
